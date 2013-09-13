8f6cbca8a0945de0dcf7c071f6246f08d468c78f
Reham-M <rehammoh@yahoo.com>
2013-09-13 20:02:06 +1000

    Add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -68,7 +68,7 @@ describe "User pages" do
       end
 
       describe "after submission" do
-        before { click_button submit}
+        before { click_button submit }
 
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
@@ -101,14 +101,21 @@ describe "User pages" do
   end
 
 
-
-describe "profile page" do
-    let(:user) {FactoryGirl.create(:user)}
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
     before { visit user_path(user) }
 
     it { should have_selector('h1', text: user.name) }
     it { should have_selector('title', text: user.name) }
-end
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end
+  end
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
@@ -130,12 +137,12 @@ end
     end
 
     describe "with valid information" do
-      let(:new_name)  { "New Name" }
+      let(:new_name) { "New Name" }
       let(:new_email) { "new@example.com" }
       before do
-        fill_in "Name",             with: new_name
-        fill_in "Email",            with: new_email
-        fill_in "Password",         with: user.password
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
         fill_in "Confirm Password", with: user.password
         click_button "Save changes"
       end
@@ -143,7 +150,7 @@ end
       it { should have_selector('title', text: new_name) }
       it { should have_selector('div.alert.alert-success') }
       it { should have_link('Sign out', href: signout_path) }
-      specify { user.reload.name.should  == new_name }
+      specify { user.reload.name.should == new_name }
       specify { user.reload.email.should == new_email }
     end
   end
